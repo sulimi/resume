80a8cc2381596eff49aa5966b8b656e810d54e6d
ycngu <snl123698745@163.com>
2018-12-04 22:46:45 +0800

    modularization

--- js/nav-active.js
+++ js/nav-active.js
@@ -1,4 +1,4 @@
-!function () {
+! function () {
     let liTags = document.querySelectorAll('nav.menu> ul > li')
     for (let i = 0; i < liTags.length; i++) {
         liTags[i].onmouseenter = function (x) {
---
+++ js/welcome.js
@@ -0,0 +1,6 @@
+! function () {
+    var welcome1 = document.querySelector('.welcome1')
+    setTimeout(() => {
+        welcome1.classList.remove('active')
+    }, 1500)
+}.call()
