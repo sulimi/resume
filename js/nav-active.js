6c4c96ed89eaa98470e88ab50acbdb4c8cdcf70b
ycngu <snl123698745@163.com>
2018-12-04 22:33:22 +0800

    modularization

---
+++ js/nav-active.js
@@ -0,0 +1,11 @@
+!function () {
+    let liTags = document.querySelectorAll('nav.menu> ul > li')
+    for (let i = 0; i < liTags.length; i++) {
+        liTags[i].onmouseenter = function (x) {
+            x.target.classList.add('active')
+        }
+        liTags[i].onmouseleave = function (x) {
+            x.target.classList.remove('active')
+        }
+    }
+}.call()
